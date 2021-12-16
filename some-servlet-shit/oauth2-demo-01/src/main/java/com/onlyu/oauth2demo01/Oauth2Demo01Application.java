3bbd3ac68f8e0bc4bdb5cc5492e32e4dbaaa9b10
Andy <duyanh.nguyen@inapps.net>
2021-12-16 23:34:45 +0700

    .

---
+++ some-servlet-shit/oauth2-demo-01/src/main/java/com/onlyu/oauth2demo01/Oauth2Demo01Application.java
@@ -0,0 +1,13 @@
+package com.onlyu.oauth2demo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2Demo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2Demo01Application.class, args);
+    }
+
+}
